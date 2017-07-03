cb7b8e6f080c00bcbded6399cdc088f14a81b1b2
Brian M. Carlson <brian.m.carlson@gmail.com>
2017-07-03 10:27:06 -0400

    Work on connection timeout

---
+++ test/connection-timeout.js
@@ -0,0 +1,15 @@
+'use strict'
+const co = require('co')
+const expect = require('expect.js')
+
+const describe = require('mocha').describe
+const it = require('mocha').it
+
+const Pool = require('../')
+
+describe('connection timeout', () => {
+  it('should be a thing', () => {
+    return Promise.reject(new Error('Should support idle timeout'))
+  })
+})
+
